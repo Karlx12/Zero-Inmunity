27efc0b04a596e8e9c60dd07f05da9d60dcd941c
RONALD FABRIZIO HURTADO RAMOS <maxx1212_2@hotmail.com>
2026-04-26 23:08:48 -0500

    feat: Implementation in common class

--- common/src/main/java/hrcode/labs/zeroinmunity/CommonClass.java
+++ common/src/main/java/hrcode/labs/zeroinmunity/CommonClass.java
@@ -1,5 +1,6 @@
 package hrcode.labs.zeroinmunity;
 
+import hrcode.labs.zeroinmunity.config.Config;
 import hrcode.labs.zeroinmunity.platform.Services;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Items;
@@ -14,18 +15,15 @@ public class CommonClass {
     // write the majority of your code here and load it from your loader specific projects. This example has some
     // code that gets invoked by the entry point of the loader specific projects.
     public static void init() {
-
-        Constants.LOG.info("Hello from Common init on {}! we are currently in a {} environment!", Services.PLATFORM.getPlatformName(), Services.PLATFORM.getEnvironmentName());
-        Constants.LOG.info("The ID for diamonds is {}", BuiltInRegistries.ITEM.getKey(Items.DIAMOND));
-
         // It is common for all supported loaders to provide a similar feature that can not be used directly in the
         // common code. A popular way to get around this is using Java's built-in service loader feature to create
         // your own abstraction layer. You can learn more about this in our provided services class. In this example
         // we have an interface in the common code and use a loader specific implementation to delegate our call to
         // the platform specific approach.
-        if (Services.PLATFORM.isModLoaded("examplemod")) {
+        Config.init();
+        if (Services.PLATFORM.isModLoaded("zeroinmunity")) {
 
-            Constants.LOG.info("Hello to examplemod");
+            Constants.LOG.info("zeroinmunity is enabled!");
         }
     }
 }
