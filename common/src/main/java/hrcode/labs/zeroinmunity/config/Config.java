1a8406cbc60c72845df4a12af60152db025fb302
RONALD FABRIZIO HURTADO RAMOS <maxx1212_2@hotmail.com>
2026-06-27 23:01:03 -0500

    fix: Change the scope to Common
    This change was made to allow a smooth use in singleplayer mode

--- common/src/main/java/hrcode/labs/zeroinmunity/config/Config.java
+++ common/src/main/java/hrcode/labs/zeroinmunity/config/Config.java
@@ -1,11 +1,6 @@
 package hrcode.labs.zeroinmunity.config;
 
-import com.iamkaf.konfig.api.v1.ConfigBuilder;
-import com.iamkaf.konfig.api.v1.ConfigHandle;
-import com.iamkaf.konfig.api.v1.ConfigScope;
-import com.iamkaf.konfig.api.v1.ConfigValue;
-import com.iamkaf.konfig.api.v1.Konfig;
-import com.iamkaf.konfig.api.v1.SyncMode;
+import com.iamkaf.konfig.api.v1.*;
 import hrcode.labs.zeroinmunity.Constants;
 
 public final class Config {
@@ -15,26 +10,30 @@ public final class Config {
     public static final ConfigValue<Integer> INVULNERABILITY_TICKS;
 
     static {
-        ConfigBuilder builder = Konfig.builder(Constants.MOD_ID, "server")
-                .scope(ConfigScope.SERVER)
-                .syncMode(SyncMode.LOGIN_AND_RELOAD)
-                .comment("Zero Immunity server settings");
 
+
+        ConfigBuilder builder = Konfig.builder(Constants.MOD_ID, "common")
+                .scope(ConfigScope.COMMON)
+                .syncMode(SyncMode.LOGIN_AND_RELOAD)
+                .comment("Zero Immunity settings");
         builder.push("damage");
 
         AFFECT_PLAYERS = builder.bool("affectPlayers", true)
                 .comment("Apply custom invulnerability logic to player victims")
                 .sync(true)
+                .restart(RestartRequirement.WORLD)
                 .build();
 
         AFFECT_MOBS = builder.bool("affectMobs", true)
                 .comment("Apply custom invulnerability logic to mob victims")
                 .sync(true)
+                .restart(RestartRequirement.WORLD)
                 .build();
 
         INVULNERABILITY_TICKS = builder.intRange("invulnerabilityTicks", 0, 0, 20)
                 .comment("Ticks of immunity after taking damage. Vanilla is 10.")
                 .sync(true)
+                .restart(RestartRequirement.WORLD)
                 .build();
 
         builder.pop();
